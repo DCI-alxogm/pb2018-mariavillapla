3570d205ddb966f9b5ca9e1d251accf6ef1c4e62
Villalobos Plascencia Maria <villalobospm2018@dci25-181.ugto.mx>
2018-08-08 09:24:46 -0500

    cree mi primer programa. cree mi primer programa. cree mi primer programa.

---
+++ semana1/programaa_hola.c
@@ -0,0 +1,14 @@
+/*
+ Esto es un comentario: lo voy a usar para escribir información que me importa o importante del programa que estoy haciendo. Por  ejemplo:
+ Este programa imprime la típica frase “hola mundo a la pantalla”
+ Creado el 06/08/2018 por Maria Villalobos Plascencia
+*/
+
+#include<stdio.h>
+int main()
+{
+printf("Hola Maria\n");
+printf("Este es el primer programa del curso\n");
+}
+/*Termine de escribir el programa*/
+
