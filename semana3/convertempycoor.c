20aaf239937ba946139ec8327c81153ed4942074
Villalobos Plascencia Maria <villalobospm2018@dci25-181.ugto.mx>
2018-08-22 09:53:37 -0500

    actualizado dia 23 de ago

---
+++ semana3/convertempycoor.c
@@ -0,0 +1,53 @@
+/*Creado el 22 de agosto de 2018 por María Villalobos primer ejercicio donde fusionamos los programas de conversion de temperaturas y coordenadas usando switch. */
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+int opcion,x,y,z,r,phi,teta,c,k,f,c1;
+printf("¿Que conversión quieres utilizar? Dame la opción que desees utilizar. \n");
+printf("(1) Para convertir temperaturas \n");
+printf("(2) Para converit coordenadas \n");
+scanf("%i",&opcion);
+
+switch(opcion)
+{
+case 1: 
+printf("Cual es la temperatura en C° del dia de hoy \n");
+scanf("%f",&c);
+
+f= (c*1.8)+32;
+k=c+273.15;
+
+printf("La temperatura en K el dia de hoy es: %f \n",k);
+printf("La temperatura en °F el dia de hoy es: %f \n",f);
+
+c1=(f-32)/1.8;
+
+printf("Finalmente la temperatura de regreso en °C el dia de hoy es: %f \n",c1);
+break;
+
+case 2:
+printf("Dame la coordenada cartesiana x \n");
+scanf("%i",&x);
+printf("Dame la coordenada cartesiana y \n");
+scanf("%i",&y);
+printf("Dame la coordenada cartesiana z \n");
+scanf("%i",&z);
+
+r=sqrt(pow(x,2)+pow(y,2)+pow(z,2));
+teta=acos(z/r);
+phi=atan(x/y);
+
+
+printf("La coordenada esférica r es %i \n",r);
+printf("La coordenada esférica φ es %i \n",phi);
+printf("La coordenada esférica θ es %i \n",teta);
+
+break;
+
+default:
+printf("Opción invalida debes de elegir entre las dos opciones \n");
+break;
+}
+return 0;
+}
--- semana3/pruebaswich.c
+++ semana3/pruebaswich.c
@@ -1,25 +1,27 @@
-#include <studio.h>
+/*Creado el 22 de agosto de 2018 por María Villalobos para probar como funciona el switch*/
+#include<stdio.h>
 int main()
 {
 int opcion;
 printf("Dame una opcion \n");
 printf("(1) Para convertir temperaturas \n");
 printf("(2) Para converit coordenadas \n");
-scanf("%i",opcion);
+scanf("%i",&opcion);
 
 switch(opcion)
 {
-case 1; 
-printf("Entre a la opcion 1");
+case 1: 
+printf("Entre a la opcion 1 \n");
 break;
 
-case 2;
-printf("Entre a la opcion 2");
+case 2:
+printf("Entre a la opcion 2 \n");
 break;
 
-default;
-printf("Opción invalida");
+default:
+printf("Opción invalida \n");
 break;
+}
 
 return 0;
 }
